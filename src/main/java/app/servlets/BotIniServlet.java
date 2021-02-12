3e14f622b966ec3958a0b2c3bb266c7c2a68b30c
ultinigg <Lutik86>
2021-02-12 22:57:13 +0300

    Buttons and commands for bot

--- src/main/java/app/servlets/BotIniServlet.java
+++ src/main/java/app/servlets/BotIniServlet.java
@@ -1,39 +1,13 @@
 package app.servlets;
 
 
-import app.model.Bot;
-import app.model.Configure;
-import jdk.internal.org.xml.sax.InputSource;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-//import javax.swing.text.Document;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Attr;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.StringReader;
 
-import static app.model.Bot.InitBot;
-import static app.model.Bot.IsBotIni;
 
 public class BotIniServlet extends HttpServlet {
     @Override
