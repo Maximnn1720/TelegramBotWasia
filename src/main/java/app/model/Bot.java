6d30aad5105ac610f074ff5c306bee286adc573c
ultinigg <Lutik86>
2021-02-16 21:18:39 +0300

    Add log4j and some debug to Bot.java

--- src/main/java/app/model/Bot.java
+++ src/main/java/app/model/Bot.java
@@ -2,21 +2,21 @@ package app.model;
 
 
 import app.Commands.HelpCommand;
+import lombok.extern.log4j.Log4j;
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
-import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-
-
+@Log4j
 public class Bot extends TelegramLongPollingCommandBot {
 
-    private Bot(){
+    private Bot() {
         this.RegisterCommand();
     }
+
     /**
      * Метод для приема сообщений.
      *
@@ -27,11 +27,17 @@ public class Bot extends TelegramLongPollingCommandBot {
 //        String message = update.getMessage().getText();
 //        sendMsg(update.getMessage().getChatId().toString(), message, update.getMessage().getMessageId());
 //    }
-
     @Override
     public void processNonCommandUpdate(Update update) {
         String message = update.getMessage().getText();
-        sendMsg(update.getMessage().getChatId().toString(), message, update.getMessage().getMessageId());
+        log.debug("Bot received message.");
+        if (message.intern() == "Maksik") {
+            sendMsg(update.getMessage().getChatId().toString(), "Maks - C# master EEE! ", update.getMessage().getMessageId());
+            log.debug("Maksik krut!");
+        } else {
+            sendMsg(update.getMessage().getChatId().toString(), message, update.getMessage().getMessageId());
+            log.debug("Bot answered the message.");
+        }
     }
 
     /**
@@ -46,10 +52,13 @@ public class Bot extends TelegramLongPollingCommandBot {
         sendMessage.setChatId(chatId);
         sendMessage.setReplyToMessageId(ID);
         sendMessage.setText(s);
+        log.debug("Bot prepared message to send.");
         try {
             execute(sendMessage);
+            log.debug("Bot send message.");
         } catch (TelegramApiException e) {
             e.printStackTrace();
+            log.debug("Something goes wrongggggg.");
         }
     }
 
@@ -86,7 +95,6 @@ public class Bot extends TelegramLongPollingCommandBot {
     }
 
 
-
     private static Bot instance = null;
 
     public static void InitBot() {
@@ -97,22 +105,26 @@ public class Bot extends TelegramLongPollingCommandBot {
                 String BotToken = Configure.GetSetting("BotToken");
 
                 Bot.setBotUsername(BotUsername);
+                log.debug("Bot set Username.");
                 Bot.setBotToken(BotToken);
+                log.debug("Bot set Token.");
 
                 instance = new Bot();
+                log.debug("Bot set instance.");
                 TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
                 telegramBotsApi.registerBot(instance);
-
-
+                log.debug("Bot register instance.");
             } catch (Exception e) {
                 e.printStackTrace();
+                log.debug("Something goes wrong! By register.");
             }
         }
     }
 
-    public void RegisterCommand(){
+    public void RegisterCommand() {
         HelpCommand helpCommand = new HelpCommand();
         register(helpCommand);
+        log.debug("Help command registered.");
     }
 
     public static boolean IsBotIni() {
